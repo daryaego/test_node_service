a283f847ebe7bf53b4b264f279d1c69f8f4fc200
edv <edv@barl.ru>
2021-03-17 15:25:08 +0300

    Added 500 respose function

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const service = http.createServer((request, response) => {
     notFound(response);
 });
 
+const internalServerError = (error, response) => {
+    response.statusCode = 500;
+    response.write(JSON.stringify(error));
+    response.end();
+}
+
 const notFound = (response) => {
     response.statusCode = 404
     response.write(`Sorry! I don't understand`);
